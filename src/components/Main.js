fd4bb3811bb0bd5bdda59e00da85ede1ee1ad1d7
mayurkolhe1995 <mayurkolhe1995@csu.fullerton.edu>
2023-05-03 18:12:07 -0700

    committing changes for the view file

--- src/components/Main.js
+++ src/components/Main.js
@@ -41,59 +41,105 @@ export default function Main(props) {
   return (
     <div className="container text-center  mt-3 mb-5">
       <Link to="uploadfiles">
-        <button style={{ width: '30%' }} className="btn btn-lg btn-outline-warning mt-4 mb-2">Upload Files Now!</button>
+        <button
+          style={{ width: "30%" }}
+          className="btn btn-lg btn-outline-warning mt-4 mb-2"
+        >
+          Upload Files Now!
+        </button>
       </Link>
       <div className="row">
         <div className="col bg-dark bg-gradient" style={{ borderRadius: 40 }}>
           <section className="m-5">
             <h1>All Files</h1>
-
-            <div class="tbl-header" >
-              <table cellpadding="0" cellspacing="0" border="0" style={{ width: '100%' }}>
+            <div class="tbl-header">
+              <table
+                cellpadding="0"
+                cellspacing="0"
+                border="0"
+                style={{ width: "100%" }}
+              >
                 <thead>
                   <tr className="bg-dark text-white">
-                    <th scope="col" style={{ width: '20%' }}>name</th>
-                    <th scope="col" style={{ width: '32%' }}>description</th>
-                    <th scope="col" style={{ width: '8%' }}>type</th>
-                    <th scope="col" style={{ width: '8%' }}>size</th>
-                    <th scope="col" style={{ width: '12%' }}>date</th>
-                    <th scope="col" style={{ width: '12%' }}>View File</th>
-                    <th scope="col" style={{ width: '8%' }}>Delete</th>
+                    <th scope="col" style={{ width: "20%" }}>
+                      name
+                    </th>
+                    <th scope="col" style={{ width: "32%" }}>
+                      description
+                    </th>
+                    <th scope="col" style={{ width: "8%" }}>
+                      type
+                    </th>
+                    <th scope="col" style={{ width: "8%" }}>
+                      size
+                    </th>
+                    <th scope="col" style={{ width: "12%" }}>
+                      date
+                    </th>
+                    <th scope="col" style={{ width: "12%" }}>
+                      View File
+                    </th>
+                    <th scope="col" style={{ width: "8%" }}>
+                      Delete
+                    </th>
                   </tr>
                 </thead>
               </table>
             </div>
             <div class="tbl-content">
-              <table cellpadding="0" cellspacing="0" border="0" style={{ width: '100%' }}>
+              <table
+                cellpadding="0"
+                cellspacing="0"
+                border="0"
+                style={{ width: "100%" }}
+              >
                 <tbody>
-
                   {allFiles &&
                     allFiles.map((file, key) => {
                       return (
                         <tr>
-                          <td style={{ width: '20%' }}>{file.fileName}</td>
-                          <td style={{ width: '32%' }}>{file.fileDes}</td>
-                          <td style={{ width: '8%' }}>{file.fileType}</td>
-                          <td style={{ width: '8%' }}>{convertBytes(file.fileSize)}</td>
-                          <td style={{ width: '12%' }}>{timeConverter(file.uploadTime)}</td>
-                          <td style={{ width: '12%' }}><a href={"https://ipfs.infura.io/ipfs/" + file.fileHash} rel="noopener noreferrer" target="_blank" className="btn btn-primary">View</a></td>
-                          <td style={{ width: '8%' }}>
-                            <button onClick={() => { deleteFile0(file.fileId.toNumber()) }} className="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+                          <td style={{ width: "20%" }}>{file.fileName}</td>
+                          <td style={{ width: "32%" }}>{file.fileDes}</td>
+                          <td style={{ width: "8%" }}>{file.fileType}</td>
+                          <td style={{ width: "8%" }}>
+                            {convertBytes(file.fileSize)}
+                          </td>
+                          <td style={{ width: "12%" }}>
+                            {timeConverter(file.uploadTime)}
+                          </td>
+                          <td style={{ width: "12%" }}>
+                            <a
+                              href={
+                                "https://dataden.infura-ipfs.io/ipfs/" +
+                                file.fileHash
+                              }
+                              rel="noopener noreferrer"
+                              target="_blank"
+                              className="btn btn-primary"
+                            >
+                              View
+                            </a>
+                          </td>
+                          <td style={{ width: "8%" }}>
+                            <button
+                              onClick={() => {
+                                deleteFile0(file.fileId.toNumber());
+                              }}
+                              className="btn btn-outline-danger"
+                            >
+                              <i class="bi bi-trash"></i>
+                            </button>
                           </td>
                         </tr>
-                      )
+                      );
                     })}
-
-
                 </tbody>
               </table>
-
             </div>
           </section>
         </div>
       </div>
     </div>
-
-  )
+  );
 }
 
